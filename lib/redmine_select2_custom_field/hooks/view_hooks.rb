f4586904081d402fa0518865c69e9b5754f3e9f4
Stephen Ostrow <contact@stephenostrow.com>
2015-02-05 20:38:25 -0500

    Use the form hook rather tha page hook
    This will allow the form field to be initiated after all form onchange
    actions.

--- lib/redmine_select2_custom_field/hooks/view_hooks.rb
+++ lib/redmine_select2_custom_field/hooks/view_hooks.rb
@@ -1,6 +1,6 @@
 module RedmineSelect2CustomField::Hooks
   class ViewHooks < Redmine::Hook::ViewListener
-    render_on :view_layouts_base_html_head,
+    render_on :view_issues_form_details_bottom,
               :partial => 'redmine_select2_custom_field/includes'
   end
 end
